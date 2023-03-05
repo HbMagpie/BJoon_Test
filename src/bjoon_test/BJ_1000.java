927a03456fa0c91031ed50f6f1debe781479a98a
HbMagpie <119773533+HbMagpie@users.noreply.github.com>
2023-03-05 19:02:39 +0900

    Add files via upload

---
+++ src/bjoon_test/BJ_1000.java
@@ -0,0 +1,15 @@
+package bjoon_test;
+
+import java.util.Scanner;
+
+public class BJ_1000 {
+	public static void main(String[] args) {
+		// 1000
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+				
+		System.out.println(a+b);
+		sc.close();
+	}
+}
