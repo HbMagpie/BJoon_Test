123d21ecf8a9c4d7af929869b745b415fa42ba1b
HbMagpie <119773533+HbMagpie@users.noreply.github.com>
2023-03-06 22:23:19 +0900

    Add files via upload

---
+++ src/bjoon_test/BJ_10869.java
@@ -0,0 +1,19 @@
+package bjoon_test;
+
+import java.util.Scanner;
+
+public class BJ_10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		// 출력 결과 정수형으로 나와야 함.
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);	
+		System.out.println(a%b);
+		sc.close();
+	}
+}
