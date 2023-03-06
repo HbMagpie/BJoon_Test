d97f1596dceb8016a3cfc9eb1c6ff7b096210478
HbMagpie <119773533+HbMagpie@users.noreply.github.com>
2023-03-07 00:57:46 +0900

    Add files via upload

---
+++ src/bjoon_test/BJ_10171.java
@@ -0,0 +1,10 @@
+package bjoon_test_01;
+
+public class BJ_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");	// "\" 출력 = 2번 입력
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
