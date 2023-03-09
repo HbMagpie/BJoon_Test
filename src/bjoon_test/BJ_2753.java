fae96a62e058a1a6e1dc0bf336de81c0473f0df5
HbMagpie <119773533+HbMagpie@users.noreply.github.com>
2023-03-09 23:54:25 +0900

    Add files via upload

---
+++ src/bjoon_test/BJ_2753.java
@@ -0,0 +1,21 @@
+package bjoon_test_02;
+
+import java.util.Scanner;
+
+public class BJ_2753 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(a % 4 == 0 && a % 100 != 0) {
+			System.out.println("1");
+		}
+		else if(a % 400 == 0) {
+			System.out.println("1");
+		}
+		else {
+			System.out.println("0");
+		}
+		sc.close();
+	}
+}
