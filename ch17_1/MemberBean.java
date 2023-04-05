323c69b1f7b1802f5e22922081f860cb567045f5
Han Bit <119773533+HbMagpie@users.noreply.github.com>
2023-04-05 13:04:05 +0900

    scr 파일

---
+++ ch17_1/MemberBean.java
@@ -0,0 +1,104 @@
+package ch17_1;
+
+public class MemberBean {
+
+	private int    numb;
+	private String usid;
+	private String uspw;
+	private String name;
+	private String gend;
+	private String brth;
+	private String mail;
+	private String post;
+	private String addr;
+	private String hobb[];
+	private String jobb;
+
+	public int getNumb() {
+		return numb;
+	}
+
+	public void setNumb(int numb) {
+		this.numb = numb;
+	}
+
+	public String getUsid() {
+		return usid;
+	}
+
+	public void setUsid(String usid) {
+		this.usid = usid;
+	}
+
+	public String getUspw() {
+		return uspw;
+	}
+
+	public void setUspw(String uspw) {
+		this.uspw = uspw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGend() {
+		return gend;
+	}
+
+	public void setGend(String gend) {
+		this.gend = gend;
+	}
+
+	public String getBrth() {
+		return brth;
+	}
+
+	public void setBrth(String brth) {
+		this.brth = brth;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String[] getHobb() {
+		return hobb;
+	}
+
+	public void setHobb(String[] hobb) {
+		this.hobb = hobb;
+	}
+
+	public String getJobb() {
+		return jobb;
+	}
+
+	public void setJobb(String jobb) {
+		this.jobb = jobb;
+	}	
+}
